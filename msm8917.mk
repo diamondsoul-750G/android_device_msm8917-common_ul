24dbd3b89f1fadc0b462c588d2a95170f7d4135c
diamondsoul-750G <n.shetti65@gmail.com>
2023-08-25 14:46:41 +0530

    removed snap overlay

--- msm8917.mk
+++ msm8917.mk
@@ -113,8 +113,7 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.5-service.msm8917 \
-    Snap 
+    android.hardware.camera.provider@2.5-service.msm8917 
     
 # Charger
 PRODUCT_PACKAGES += \
